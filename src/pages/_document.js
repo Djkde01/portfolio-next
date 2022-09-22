94326c4b57a67af9c299ddf35585db0735c8a64f
Djkde01 <sergio.estrella@utp.edu.co>
2022-09-22 17:19:37 -0500

    feat(meta tags)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -35,6 +35,41 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <title>Sergio Estrella - Djkde</title>
+          <meta
+            property="og:title"
+            content="Sergio Estrella's Personal Page - @djkde"
+            key="title"
+          />
+          <meta
+            property="og:description"
+            content="Web Developer, Community Builder, Teacher & Eternal Student"
+          />
         </Head>
         <body>
           <Main />
